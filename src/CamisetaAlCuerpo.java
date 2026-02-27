d26e3095e1004573db0ef35419da69dce569fafc
Juan Jose <jjperezbravo123@gmail.com>
2026-02-27 16:01:26 -0500

    Update CamisetaAlCuerpo.java

--- src/CamisetaAlCuerpo.java
+++ src/CamisetaAlCuerpo.java
@@ -1,6 +1,9 @@
 
-public interface Camiseta {
-
-	void description();
+public class CamisetaAlCuerpo implements Camiseta {
 	
+	@Override 
+	public void description() {
+		 System.out.println("Camiseta al cuerpo");
+		
+	}
 }
