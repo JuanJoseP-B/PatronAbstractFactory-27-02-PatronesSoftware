d7068945ef8951f52fdb967b2596d04da1390bb7
Juan Jose <jjperezbravo123@gmail.com>
2026-02-27 16:09:38 -0500

    Update TiendaRopa.java

--- src/TiendaRopa.java
+++ src/TiendaRopa.java
@@ -1,4 +1,16 @@
+public class TiendaRopa { 
 
-public class TiendaRopa {
+    private OutfitFactory factory;
 
+    public TiendaRopa(OutfitFactory factory) {
+        this.factory = factory;
+    }
+
+    public void mostrarOutfit() {
+        Jean jean = factory.crearJean();
+        Camiseta camiseta = factory.crearRemera();
+
+        jean.descripcion();
+        Camiseta.descripcion();
+    }
 }
