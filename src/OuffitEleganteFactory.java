880b1079cb1012d4dc77698c821f10cc21ce55eb
Juan Jose <jjperezbravo123@gmail.com>
2026-02-27 16:11:36 -0500

    Update OuffitEleganteFactory.java

--- src/OuffitEleganteFactory.java
+++ src/OuffitEleganteFactory.java
@@ -1,4 +1,14 @@
 
 public class OuffitEleganteFactory {
+  @Override
+    public Jean crearJean() {
+        return new JeanRecto();
+    }
+
+    @Override
+    public Camiseta crearCamiseta() {
+        return new CamisetaAlCuerpo();
+    }
 
 }
+
